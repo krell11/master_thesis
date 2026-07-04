08aea70da793b088f38193a50b13727962036358
krell11 <11bobin.kiryusha@gmail.com>
2026-07-05 00:30:40 +0300

    some preparations stuff

--- src/scripts/load_model.py
+++ src/scripts/load_model.py
@@ -1,5 +1,6 @@
 from huggingface_hub import snapshot_download
-snapshot_download(
-    repo_id="Qwen/Qwen2.5-7B-Instruct",
-    local_dir="D:/models/Qwen2.5-7B-Instruct",
-)
+
+if __name__ == "__main__":
+    repo_id = "Qwen/Qwen3.5-4B" 
+    local_dir = "E:\\university\\masters\\models\\" 
+    snapshot_download(repo_id=repo_id, local_dir=local_dir)
