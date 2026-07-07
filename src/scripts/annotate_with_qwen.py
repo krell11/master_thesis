9f65922a5ee7c0ba1085a3ac35917d210f9371fc
krell11 <11bobin.kiryusha@gmail.com>
2026-07-08 00:37:44 +0300

    fix annotation

--- src/scripts/annotate_with_qwen.py
+++ src/scripts/annotate_with_qwen.py
@@ -1,20 +1,28 @@
 import json
-from datasets import load_dataset
 from transformers import AutoTokenizer
 from vllm import LLM, SamplingParams
 
-tokenizer =   AutoTokenizer.from_pretrained("../../models/", trust_remote_code=True)
-NUM_SEQS: int = 10
+
+model_path  = '/workspace/models/'
+tokenizer =   AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+
 
 
 def process_dataset(dataset: list, outputs_path: str):
-    sampling_params = SamplingParams(n = 1, top_p=1, temperature=0.3, seed=999, max_tokens=512)
-    llm = LLM(model="../../models/")
+    sampling_params = SamplingParams(n = 1, top_p=1, temperature = 0.3, seed = 999, max_tokens = 128)
+    llm = LLM(
+        model=model_path,
+        gpu_memory_utilization=0.9,
+        max_model_len=4096,
+        max_num_seqs=32,
+        language_model_only=True,
+        trust_remote_code=True,
+    )
     outputs = llm.generate(dataset, sampling_params)
-    with open(outputs_path, "w", encoding="utf-8"):
+    with open(outputs_path, "w", encoding="utf-8") as f:
         for output in outputs:
-            generated = output.outputs[0].text
-            
+            generated_sample= output.outputs[0].text
+            f.write(json.dumps({"qas":generated_sample}) + "\n")
             
 
 def create_dataset(dataset_path: str):
@@ -23,7 +31,7 @@ def create_dataset(dataset_path: str):
         for line in f:
             sample = json.loads(line)
             paper_name = sample["title"]
-            for question in sample["qas"]:
+            for question in sample["qas"]["question"]:
                 prompt = f"""
                             You are given a question that adresses a paper named {paper_name}, your task is to rephrase the question to make it adress the paper name.
                             Here is the question you need to rephrase {question} 
@@ -35,11 +43,12 @@ def create_dataset(dataset_path: str):
                                 "role": "user",
                                 "content": prompt,
                             },
-                            ], tokenize=False, add_generation_prompt=False)
+                            ], tokenize=False, add_generation_prompt=True, enable_thinking=False)
                 
                 prompts.append(messages)
     return prompts
 
 
 if __name__ == "__main__":
-    create_dataset("/mnt/e/university/masters/data/train_data/train.jsonl")
+    dataset = create_dataset("/workspace/data/train_data/train.jsonl")
+    process_dataset(dataset,"/workspace/data/train_data/generated.jsonl")
